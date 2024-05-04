3823c917af8a41e461c43f015daebd494af182db
nbazile <narmernbazile@gmail.com>
2024-05-04 16:56:02 -0400

    reorganize

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import sys
 
 
 
@@ -29,10 +30,10 @@ def reaction_diffusion(da: float, # diffusion rate for A
                        db: float, # diffusion rale for B
                         F: float, # feed rate
                         k: int,   # kill rate
-                        n: int,   # number of cells; nxn
-                        h: int,   # approximation interval
-                       nt: int,   # number of timesteps to sumulate
-                       dt: float  # magnitude of each timestep
+                        n: int   = 64,         # number of cells; nxn
+                        h: int   = 2,     # approximation interval
+                       nt: int   = 20000, # number of timesteps to sumulate
+                       dt: float = 1      # magnitude of each timestep
                         ):
   
   # initalized concentrations of chemicals A and B represented as nxn matrices
@@ -55,10 +56,10 @@ def reaction_diffusion(da: float, # diffusion rate for A
   return A, B
 
 
-def save_tile(matrix, out_img_path):
+def save_tile(matrix, out_img_path, _cmap):
   fig, ax = plt.subplots(tight_layout=True)
   plt.axis('off')
-  ax.imshow(matrix, interpolation='lanczos', cmap="binary")
+  ax.imshow(matrix, interpolation='lanczos', cmap=_cmap)
   fig.savefig(out_img_path, bbox_inches='tight', pad_inches=0)
 
 
@@ -85,6 +86,26 @@ def contrast_img(in_img_path, out_img_path, light_color, dark_color):
   output_image = Image.fromarray(image_array)
   output_image.save(out_img_path)
 
+def merge_and_contrast_img(in_imgA_path, in_imgB_path, out_img_path):
+  input_imageA = Image.open(in_imgA_path)
+  input_imageB = Image.open(in_imgB_path)
+  imageA_array = np.array(input_imageA)
+  imageB_array = np.array(input_imageB)
+  if not len(imageA_array) == len(imageB_array):
+    print("error: image A and image B are different dimensions")
+    sys.exit()
+  N = len(imageA_array)
+  output_array = np.zeros((N,N,4))
+  for i in range(N):
+    for j in range(N):
+      output_array[i][j][0] = int(imageA_array[i][j][0])  # adopt the 'reds' from image A
+      output_array[i][j][1] = 0 # don't use the green channel
+      output_array[i][j][2] = int(imageB_array[i][j][2])
+      output_array[i][j][3] = 255 # we want this pixel to be fully opaque 
+
+  output_image = Image.fromarray(output_array, 'RGBA')
+  output_image.save(out_img_path)
+
 
 def tile_img(in_img_path, out_img_path, scale_factor):
   input_image = Image.open(in_img_path)
@@ -103,17 +124,42 @@ def tile_img(in_img_path, out_img_path, scale_factor):
   output_image.save(out_img_path)
 
 
+def eggs(): 
+  return reaction_diffusion(0.30, 0.10, 0.027, 0.040)
+
+def standard(): 
+  return reaction_diffusion(0.20, 0.10, 0.025, 0.056)
+
+def circles(): 
+  return reaction_diffusion(0.10, 0.20, 0.025, 0.056)
+
+def texture(): 
+  return reaction_diffusion(0.55, 0.08, 0.027, 0.040)
+  
+def big_eggs():
+  return reaction_diffusion(0.10, 0.20, 0.025, 0.040)
+
 def main():
   WHITE  = (255, 255, 255)
   BLACK  = (0, 0, 0)
   GREEN  = (0, 128, 0)
+  RED    = (128, 0, 0)
   YELLOW = (255, 255, 0)
   PINK   = (255, 192, 203)
-  # A, B=reaction_diffusion(0.20, 0.10, 0.025, 0.056, 64, 2, 20000, 1) # standard
-  A, B = reaction_diffusion(0.30, 0.10, 0.027, 0.040, 64, 2, 20000, 1) # eggs
-  save_tile(A, "./_tile.png")
-  contrast_img("./_tile.png", "_processed_tile.png", WHITE, PINK)
-  tile_img("./_processed_tile.png", "./wallpaper.png", 8)
+
+  patterns = [standard, texture, circles, eggs, big_eggs]
+  for idx, pattern in enumerate(patterns):
+    A, B = pattern()
+    save_tile(A, f"./.rd/_tile{idx}.png", "binary")
+    contrast_img(f"./.rd/_tile{idx}.png", f"./.rd/_processed_tile{idx}.png", WHITE, BLACK)
+    tile_img(f"./.rd/_processed_tile{idx}.png", f"./.rd/wallpaper{idx}.png", 9)
+  
+  # save_tile(A, "./_tileA.png", "Reds")
+  # save_tile(B, "./_tileB.png", "Blues")
+  # merge_and_contrast_img("./_tileA.png", "./_tileB.png", "_processed_tile.png")
+  # save_tile(A, "./_tile.png", "binary")
+  # contrast_img("./_tile.png", "_processed_tile.png", PINK, RED)
+  # tile_img("./_processed_tile.png", "./wallpaper.png", 8)
 
 
 if __name__ == "__main__": main()
